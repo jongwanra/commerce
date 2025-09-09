6e007f73868840fd11b91d1501e35d46e904622d
jongwanra <najongsi@gmail.com>
2025-09-10 07:09:35 +0900

    feat: querydsl 설정

--- build.gradle.kts
+++ build.gradle.kts
@@ -48,9 +48,34 @@ dependencies {
 
     // Open API
     implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.7.0")
+
+    /**
+     * querydsl
+     */
+
+    implementation("com.querydsl:querydsl-jpa:5.0.0:jakarta")
+    annotationProcessor("com.querydsl:querydsl-apt:5.0.0:jakarta")
+    annotationProcessor("jakarta.annotation:jakarta.annotation-api")
+    annotationProcessor("jakarta.persistence:jakarta.persistence-api")
 }
+
+/**
+ * querydsl build options
+ */
+val querydslSrcDir = "src/main/generated"
+sourceSets["main"].java.srcDir(querydslSrcDir)
 
 tasks.withType<Test> {
     useJUnitPlatform()
     systemProperty("user.timezone", "UTC")
+}
+
+tasks.withType<JavaCompile> {
+    options.generatedSourceOutputDirectory = file(querydslSrcDir)
+}
+
+tasks.named("clean") {
+    doLast {
+        file(querydslSrcDir).deleteRecursively()
+    }
 }
