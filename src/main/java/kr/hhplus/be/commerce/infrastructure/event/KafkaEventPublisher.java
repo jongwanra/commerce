7c282c58f1ba0f6fc5d5b8f67c9d85939b51209e
jongwanra <najongsi@gmail.com>
2025-12-25 16:18:22 +0900

    fix: KafkaEventPublisher 비동기 전송 문제 피드백 반영

--- src/main/java/kr/hhplus/be/commerce/infrastructure/event/KafkaEventPublisher.java
+++ src/main/java/kr/hhplus/be/commerce/infrastructure/event/KafkaEventPublisher.java
@@ -1,6 +1,10 @@
 package kr.hhplus.be.commerce.infrastructure.event;
 
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.SendResult;
 import org.springframework.stereotype.Component;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
@@ -24,10 +28,19 @@ public class KafkaEventPublisher implements ExternalEventPublisher {
 	public void publish(Event event) {
 		try {
 			final String topic = eventTopicResolver.resolve(event);
-			kafkaTemplate.send(topic, event.key(), mapper.writeValueAsString(event));
+			SendResult<String, String> result = kafkaTemplate.send(topic, event.key(),
+					mapper.writeValueAsString(event))
+				.get(5, TimeUnit.SECONDS);// 5초 타임아웃
+
+			log.debug("[KafkaEventPublisher] 메시지 발행 성공. Topic={}, Partition={}, Offset={}", topic,
+				result.getRecordMetadata().partition(), result.getRecordMetadata().offset());
+
 		} catch (JsonProcessingException e) {
 			log.error("[KafkaEventPublisher] Event 객체를 JSON 문자열로 직렬화하는데 에러가 발생했습니다.", e);
 			throw new CommerceException(e.getMessage());
+		} catch (TimeoutException e) {
+			log.error("[KafkaEventPublisher] Kafka 메시지 발행 타임아웃", e);
+			throw new CommerceException("이벤트 발행 타임아웃");
 		} catch (Exception e) {
 			log.error("[KafkaEventPublisher] 이벤트를 발행하는데 예상하지 못한 에러가 발생했습니다.", e);
 			throw new CommerceException(e.getMessage());
