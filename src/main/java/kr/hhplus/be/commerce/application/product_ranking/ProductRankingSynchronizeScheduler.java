946ab1fa45ad51330e38affca6e3c58b21f6b1c9
jongwanra <najongsi@gmail.com>
2026-01-01 17:32:38 +0900

    fix: Scheduler Profile 설정 누락 피드백 반영

--- src/main/java/kr/hhplus/be/commerce/application/product_ranking/ProductRankingSynchronizeScheduler.java
+++ src/main/java/kr/hhplus/be/commerce/application/product_ranking/ProductRankingSynchronizeScheduler.java
@@ -5,6 +5,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
+import org.springframework.context.annotation.Profile;
 import org.springframework.data.redis.RedisConnectionFailureException;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
@@ -23,6 +24,7 @@ import lombok.extern.slf4j.Slf4j;
 @Component
 @RequiredArgsConstructor
 @Slf4j
+@Profile({"local", "prod"})
 public class ProductRankingSynchronizeScheduler {
 	private static final int FIVE_MINUTES = 5 * 60 * 1000;
 
